eeaab246a3fb7f471103df9d5bca98bcdb8ecfd2
Steven <stevenbirdbrown1998@gmail.com>
2017-12-10 14:56:10 +0800

    更改了if，但是还是很慢

--- MyCPU/MyCPU.srcs/sources_1/new/cu.v
+++ MyCPU/MyCPU.srcs/sources_1/new/cu.v
@@ -144,22 +144,26 @@ module cu(
         end
         4'b0101:
         begin
-            if(op==adc||op==sbb||op==mul||op==div||op==orr||op==andd||op==xorr||
-            op==test||op==cmp||op==nott||op==shl||op==sar||op==shr||op==mov)
+            //if(op==adc||op==sbb||op==mul||op==div||op==orr||op==andd||op==xorr||
+            //op==test||op==cmp||op==nott||op==shl||op==sar||op==shr||op==mov)
+            casex(op)
+            adc, sbb, mul, div, orr, andd, xorr, test, cmp, nott, shl, sar, shr, mov:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h40;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==addi||op==movil||op==movih)
+            //else if(op==addi||op==movil||op==movih)
+            addi, movil, movih:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h41;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==jr||op==jrc||op==jrnc||op==jrz||op==jrnz)
+            //else if(op==jr||op==jrc||op==jrnc||op==jrz||op==jrnz)
+            jr, jrc,jrnc, jrz, jrnz:
             begin
                 if(op==jr||((op==jrc)&&flag_in[3])||((op==jrnc)&&(!flag_in[3]))||
                 ((op==jrz)&&flag_in[2])||((op==jrnz)&&(!flag_in[2])))
@@ -178,178 +182,207 @@ module cu(
                     {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
                 end
             end
-            else if(op==load||op==in)
+            //else if(op==load||op==in)
+            load, in:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h40;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h28;
             end
-            else if(op==store||op==out)
+            //else if(op==store||op==out)
+            store, out:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h40;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h1e;
             end
-            else if(op==push)
+            //else if(op==push)
+            push:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h40;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h40;
             end
-            else if(op==pop)
+            //else if(op==pop)
+            pop:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h40;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h38;
             end
-            else if(op==clc||op==stc||op==nop)
+            //else if(op==clc||op==stc||op==nop)
+            clc, stc, nop:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h40;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==halt)
+            //else if(op==halt)
+            halt:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0100000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
+            endcase
             state=4'b0110;
         end
         4'b0110:
         begin
-            if(op==adc||op==sbb||op==mul||op==div||op==addi||op==orr||op==andd||
-            op==xorr||op==test||op==cmp||op==nott||op==shl||op==shr||op==sar||op==mov||
-            op==movil||op==movih||op==clc||op==stc)
+            //if(op==adc||op==sbb||op==mul||op==div||op==addi||op==orr||op==andd||
+            //op==xorr||op==test||op==cmp||op==nott||op==shl||op==shr||op==sar||op==mov||
+            //op==movil||op==movih||op==clc||op==stc)
+            casex(op)
+            adc, sbb, mul, div, addi, orr, andd, xorr, test, cmp, nott, shl, shr, sar,
+            mov, movil, movih, clc, stc:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==jr||op==jrc||op==jrnc||op==jrz||op==jrnz)
+            //else if(op==jr||op==jrc||op==jrnc||op==jrz||op==jrnz)
+            jr, jrc, jrnc, jrz, jrnz:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==load)
+            //else if(op==load)
+            load:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000001;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==pop)
+            //else if(op==pop)
+            pop:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000001;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h80;
             end
-            else if(op==store)
+            //else if(op==store)
+            store:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000010;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==push)
+            //else if(op==push)
+            push:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h3d;
             end
-            else if(op==nop||op==halt)
+            //else if(op==nop||op==halt)
+            nop, halt:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==in)
+            //else if(op==in)
+            in:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0010000;//10 means in
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==out)
+            //else if(op==out)
+            out:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0011000;//11 means out
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
+            endcase
             state=4'b0111;
         end
         4'b0111:    //write back to reg array and set eflags
         begin
-            if(op==adc||op==sbb||op==div||op==addi||op==shl||op==shr||op==sar)
+            //if(op==adc||op==sbb||op==div||op==addi||op==shl||op==shr||op==sar)
+            casex(op)
+            adc, sbb, div, addi, shl, shr, sar:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h04;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000100;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==mul)
+            //else if(op==mul)
+            mul:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h06;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000100;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==andd||op==orr||op==xorr||op==nott||op==mov||op==movil||op==movih)
+            //else if(op==andd||op==orr||op==xorr||op==nott||op==mov||op==movil||op==movih)
+            andd, orr, xorr, nott, mov, movil, movih:
             begin   //only write register array
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h04;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==cmp||op==test||op==clc||op==stc)//only change eflags
+            //else if(op==cmp||op==test||op==clc||op==stc)//only change eflags
+            cmp, test, clc, stc:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000100;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==nop||op==halt||op==jr||op==jrc||op==jrnc||op==jrz||op==jrnz||
-            op==store||op==out)
+            //else if(op==nop||op==halt||op==jr||op==jrc||op==jrnc||op==jrz||op==jrnz||
+            //op==store||op==out)
+            nop, halt, jr, jrc, jrnc, jrz, jrnz, store, out:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==push)
+            //else if(op==push)
+            push:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000010;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h00;
             end
-            else if(op==in) //mem->mdr
+            //else if(op==in) //mem->mdr
+            in:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000000;
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h07;
             end
-            else if(op==load||op==pop)  //mem->mdr
+            //else if(op==load||op==pop)  //mem->mdr
+            load, pop:
             begin
                 {pc_inc, pc_ena, ir_ena, reg_read1,
                 reg_read2, reg_write1, reg_write2, alu_data_sel}=8'h00;
                 {alu_ena, hlt, io, flag_set, wr_m, rd_m}=7'b0000001; //rd_m=1
                 {sp_pop, sp_push, mar_sel, mar_ena, mdr_ena, mdr_sel}=8'h07;
             end
+            endcase
             state=4'b1000;
         end
         4'b1000:
