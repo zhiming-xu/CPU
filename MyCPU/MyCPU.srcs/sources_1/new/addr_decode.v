b09fed7eb1829429961841fbd78386e13babab0f
Steven <stevenbirdbrown1998@gmail.com>
2017-12-23 20:37:40 +0800

    这是一个正确的没有加入单步调试的版本，之后创建bug分支

--- MyCPU/MyCPU.srcs/sources_1/new/addr_decode.v
+++ MyCPU/MyCPU.srcs/sources_1/new/addr_decode.v
@@ -27,11 +27,17 @@ module addr_decode(
     );
     always @ (addr)
     begin
-        casex(addr)
-        16'b0000_0000_xxxx_xxxx: {rom_sel, ram_sel}=2'b10;
-        16'b0000_0001_xxxx_xxxx: {rom_sel, ram_sel}=2'b01;
-        16'b0000_0010_xxxx_xxxx: {rom_sel, ram_sel}=2'b01;
-        default: {rom_sel, ram_sel}=2'b00;
-        endcase
+//        casex(addr)
+//        16'b0000_0000_xxxx_xxxx: {rom_sel, ram_sel}=2'b10;
+//        16'b0000_0001_xxxx_xxxx: {rom_sel, ram_sel}=2'b01;
+//        16'b0000_0010_xxxx_xxxx: {rom_sel, ram_sel}=2'b01;
+//        default: {rom_sel, ram_sel}=2'b00;
+//        endcase
+        if(addr<16'h0100)
+            {rom_sel, ram_sel}=2'b10;
+        else if(addr>=16'h0100&&addr<16'h0300)
+            {rom_sel, ram_sel}=2'b01;
+        else
+            {rom_sel, ram_sel}=2'b00;
     end
 endmodule
